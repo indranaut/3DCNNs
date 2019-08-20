59ae2386babe39339d878129e333edc843b93d2e
Ujjwal <ujjwal_j14@protonmail.ch>
2019-08-20 19:43:25 +0200

    Wrote code for building network from proto specification.
    Signed-off-by: calledbymountains <ailearner@keemail.me>

--- builders/network_builder.py
+++ builders/network_builder.py
@@ -1,5 +1,10 @@
+import tensorflow as tf
 from protos import network_pb2
+from nets.i3d import Inception3D
 
+NETOWRK_MAP = dict(
+    i3d=Inception3D
+)
 
 def parse_network_config(network_proto_config):
     numframes = network_proto_config.numframes
@@ -20,3 +25,32 @@ def parse_network_config(network_proto_config):
         num_classes=num_classes
     )
     return network_dictionary
+
+
+def build_network(network_proto_config, is_training=False):
+    network_dictionary = parse_network_config(network_proto_config)
+    network_fn = NETOWRK_MAP[network_dictionary['name']]
+    network = network_fn(num_classes=network_dictionary['num_classes'],
+                         is_training=is_training, data_format=network_dictionary['data_format'],
+                         **network_dictionary['network_parameters'])
+
+    if network_dictionary['data_format'] == 'channels_last':
+        input_shape = (None, network_dictionary['numframes'],
+                       network_dictionary['input_height'],
+                       network_dictionary['input_width'],
+                       3)
+    else:
+        input_shape = (None, network_dictionary['numframes'],3,
+                        network_dictionary['input_height'],
+                        network_dictionary['input_width'])
+
+    network.build(input_shape=input_shape)
+    tf.logging.info(network.summary())
+
+    return network
+
+
+
+
+
+
