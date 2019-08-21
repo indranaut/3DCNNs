8dcd4f9c462158ca26b50157c12b5d4002bd4a68
Ujjwal <ujjwal_j14@protonmail.ch>
2019-08-21 16:10:42 +0200

    Use MessageToDict method for parsing the proto configuration for Network message in protos/network.proto
    Signed-off-by: calledbymountains <ailearner@keemail.me>

--- builders/network_builder.py
+++ builders/network_builder.py
@@ -1,31 +1,21 @@
-import tensorflow as tf
 import logging
-from protos import network_pb2
 from nets.i3d import Inception3D
+from google.protobuf.json_format import MessageToDict
 
 NETOWRK_MAP = dict(
     i3d=Inception3D
 )
 
+
 def parse_network_config(network_proto_config):
-    numframes = network_proto_config.numframes
-    input_height = network_proto_config.input_height
-    input_width = network_proto_config.input_width
-    num_classes = network_proto_config.num_classes
-    data_format = network_proto_config.WhichOneof('data_format')
-    data_format = data_format.lower()
-    network_type = network_proto_config.WhichOneof('network_details')
-    network_parameters = network_proto_config.network_type
-    network_dictionary = dict(
-        name=network_type,
-        network_parameters=network_parameters,
-        data_format=data_format,
-        input_height=input_height,
-        input_width=input_width,
-        numframes=numframes,
-        num_classes=num_classes
-    )
-    return network_dictionary
+    network_dict = MessageToDict(message=network_proto_config,
+                                 including_default_value_fields=True,
+                                 preserving_proto_field_name=True)
+    network_name = network_proto_config.WhichOneof('network_details')
+    data_format = network_dict['data_format'].lower()
+    network_dict['data_format'] = data_format
+    network_dict['name'] = network_name
+    return network_dict
 
 
 def build_network(network_proto_config, is_training=False):
@@ -33,25 +23,19 @@ def build_network(network_proto_config, is_training=False):
     network_fn = NETOWRK_MAP[network_dictionary['name']]
     network = network_fn(num_classes=network_dictionary['num_classes'],
                          is_training=is_training, data_format=network_dictionary['data_format'],
-                         **network_dictionary['network_parameters'])
+                         **network_dictionary[network_dictionary['name']])
 
     if network_dictionary['data_format'] == 'channels_last':
-        input_shape = (None, network_dictionary['numframes'],
+        input_shape = (None, network_dictionary['num_frames'],
                        network_dictionary['input_height'],
                        network_dictionary['input_width'],
                        3)
     else:
-        input_shape = (None, network_dictionary['numframes'],3,
-                        network_dictionary['input_height'],
-                        network_dictionary['input_width'])
+        input_shape = (None, 3, network_dictionary['numframes'],
+                       network_dictionary['input_height'],
+                       network_dictionary['input_width'])
 
     network.build(input_shape=input_shape)
     logging.info(network.summary())
 
     return network
-
-
-
-
-
-
